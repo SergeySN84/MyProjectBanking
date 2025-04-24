24b275925fb510d129789d93e5ed1d01090a141e
SergeySN84 <Sergsvircuk@mail.ru>
2025-04-24 17:05:47 +0700

    Добавление фикстур и параметризаций в тестирование функции processing

--- tests/tests_processing.py
+++ tests/tests_processing.py
@@ -1,62 +1,65 @@
 import pytest
-
 from src.processing import filter_by_state, sort_by_date
 
-# Тестовые данные
-users_data = [
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-]
-
-# Тесты для функции filter_by_state
-def test_filter_by_state_default():
-    """Тест фильтрации по умолчанию (state='EXECUTED')"""
-    expected_result = [
+# Фикстура для тестовых данных
+@pytest.fixture
+def tests_data():
+    return [
         {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
-    ]
-    result = filter_by_state(users_data)
-    assert result == expected_result
-
-def test_filter_by_state_custom():
-    """Тест фильтрации с указанием state='CANCELED'"""
-    expected_result = [
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
         {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
     ]
-    result = filter_by_state(users_data, state="CANCELED")
-    assert result == expected_result
 
-def test_filter_by_state_empty():
-    """Тест фильтрации с несуществующим state"""
-    result = filter_by_state(users_data, state="UNKNOWN")
-    assert result == []
-
-# Тесты для функции sort_by_date
-def test_sort_by_date_descending():
-    """Тест сортировки по убыванию (reverse=True)"""
-    expected_result = [
-        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
-        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
+# Тесты для функции filter_by_state с параметризацией
+@pytest.mark.parametrize(
+    "state, expected",
+    [
+        # Тест фильтрации по умолчанию (state='EXECUTED')
+        ("EXECUTED", [
+            {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
+        ]),
+        # Тест фильтрации с указанием state='CANCELED'
+        ("CANCELED", [
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+        ]),
+        # Тест фильтрации с несуществующим state
+        ("UNKNOWN", [])
     ]
-    result = sort_by_date(users_data, reverse=True)
-    assert result == expected_result
+)
+def test_filter_by_state(tests_data, state, expected):
+    """Параметризованный тест фильтрации по состоянию"""
+    result = filter_by_state(tests_data, state=state)
+    assert result == expected
 
-def test_sort_by_date_ascending():
-    """Тест сортировки по возрастанию (reverse=False)"""
-    expected_result = [
-        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
-        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}
+# Тесты для функции sort_by_date с параметризацией
+@pytest.mark.parametrize(
+    "reverse, expected",
+    [
+        # Тест сортировки по убыванию (reverse=True)
+        (True, [
+            {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
+        ]),
+        # Тест сортировки по возрастанию (reverse=False)
+        (False, [
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+            {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}
+        ])
     ]
-    result = sort_by_date(users_data, reverse=False)
-    assert result == expected_result
+)
+def test_sort_by_date(tests_data, reverse, expected):
+    """Параметризованный тест сортировки по дате"""
+    result = sort_by_date(tests_data, reverse=reverse)
+    assert result == expected
 
+# Отдельный тест для пустого списка
 def test_sort_by_date_empty():
     """Тест сортировки пустого списка"""
     result = sort_by_date([])
